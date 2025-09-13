92e0d993c20f3c5d19d98007e41f91791bfe7606
Akeri <samaescii@gmail.com>
2025-09-12 19:23:11 -0600

    Feat: Configuración inicial QuickPass

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-    plugins: [
-    tailwindcss(),
-],
-})
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./index.html', './src/**/*.{js,jsx}'],
+    darkMode: 'media',
+    theme: {
+    extend: {
+        colors: {
+        brand: {
+            DEFAULT: '#540000ff',
+            light: '#e80000ff',
+            dark: '#af4040ff'
+        },
+        bg: {
+            light: '#ffffff',
+            dark: '#00171f'
+        },
+        text: {
+            light: '#0f172a',
+            dark: '#e2e8f0'
+        }
+        },
+        boxShadow: {
+        soft: '0 8px 24px rgba(2, 6, 23, 0.08)'
+        },
+        borderRadius: {
+        xl2: '1.25rem'
+        }
+    }
+    },
+    plugins: []
+}
